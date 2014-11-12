6f4a6474fc5426d5b30379c860e1ff9cb51aa135
Kashyap <ckkashyap@gmail.com>
2014-11-12 23:33:36 +0530

    rust integration done

---
+++ boot/x86_64/func.rs
@@ -0,0 +1,12 @@
+#![crate_name = "rustcode"]
+#![crate_type = "staticlib"]
+#![feature(lang_items)]
+#![no_std]
+
+#[lang="sized"]
+#[no_mangle]
+pub extern "C" fn rust_func() -> int {
+        return 255;
+}
+
+#[lang = "stack_exhausted"] extern fn stack_exhausted() {}
--- boot/x86_64/main.c
+++ boot/x86_64/main.c
@@ -1,4 +1,10 @@
 int main()
 {
+	int count = rust_func();
+	int i;
+	asm("mov $0, %eax");
+	for(i=0;i<count;i++){
+		asm("inc %rax");
+	}
 	while (1);
 }
