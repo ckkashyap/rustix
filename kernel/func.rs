c86cc7d19305734c0ba299d2587ee1623c89a258
CK Kashyap <ckk@microsoft.com>
2014-11-15 08:44:08 -0500

    removed the c file

--- kernel/func.rs
+++ kernel/func.rs
@@ -2,11 +2,17 @@
 #![crate_type = "staticlib"]
 #![feature(lang_items)]
 #![no_std]
-
 #[lang="sized"]
+
+fn kashyap () {
+}
+
 #[no_mangle]
-pub extern "C" fn rust_func() -> int {
-        return 255;
+pub extern "C" fn main()  {
+	loop {
+		kashyap();
+	}
+        //return 255;
 }
 
 #[lang = "stack_exhausted"] extern fn stack_exhausted() {}
